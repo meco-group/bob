a0a56124473fc3d246eaefe092ef673804a0b4da
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2016-04-20 16:02:52 +0200

    bootstrapping

--- setup.sh
+++ setup.sh
@@ -3,3 +3,16 @@ set -e
 
 openssl aes-256-cbc -k "$key" -in id_rsa_bob.enc -out id_rsa_bob -d
 chmod 600 id_rsa_bob
+
+openssl aes-256-cbc -k "$key" -in setup_enc.sh.enc -out setup_enc.sh -d
+chmod 700 setup_enc.sh
+
+source setup_enc.sh
+
+bob_get() {
+  scp $USER@$GATE:~/repo/$1 .
+}
+
+bob_put() {
+  scp $1 $USER@$GATE:~/repo/
+}
