c575e147e301454edad98a76f9c9c19f7197236b
jgillis <joris.gillis42@gmail.com>
2019-06-21 08:22:07 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,9 +8,6 @@ openssl aes-256-cbc -k "$key" -in setup_enc.sh.enc -out setup_enc.sh -d
 chmod 700 setup_enc.sh
 
 
-echo "debug ssl" | sudo tee -a  /etc/davfs2/davfs2.conf
-
-
 source setup_enc.sh
 
 #sudo dpkg -i https://admin.kuleuven.be/icts/services/extranet/ps-pulse-linux-9-0r3-0-b923-ubuntu-debian-64-bit.deb
@@ -18,17 +15,20 @@ source setup_enc.sh
 
 sudo apt-get install davfs2
 
+
+echo "debug ssl" | sudo tee -a  /etc/davfs2/davfs2.conf
+
 echo "mounting"
 echo "https://drives.kuleuven.be/hcwebdav/ $USER $DRIVE_PASSWORD" > secrets
 sudo bash -c "cat secrets >> /etc/davfs2/secrets"
 
 sudo mkdir -p /mnt/dav
 
-#(while true ; do sleep 60 ; echo "ping" ; done ) &
-#ssh-keyscan serveo.net >> ~/.ssh/known_hosts
-#echo -e "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDNPkKyOfNMobbgIqD0WvkBTzhEs6St8GYC7aYPT4C0eMimuUBJoQbuUeZQS3hkk9RcdG6h8z0Da70mMV82rcWgztE0obdULduOqDV0GrL6TiqaxoHGTEHPzkq7G48B48S+kmQba0rqURtMJ6SLdr/jElJhnVFC2SC8cYz4Q5BikcuX16LNhxLTxcWSq9Ug9cGO/Gc65n2tKUqPy9Ky4LEURBF3zmUftUdR7wE0GUahsti1aDAQGNbB2ccrpUZWQkVxTzol4ABaCEDo1wBPuug4CBuy+kaIj2aDi01z5D52ED2jsa+g38PStV8zJFFrJJMY10RwigMhHVoOfd7bMF3H jgillis@jglab-work" >> ~/.ssh/authorized_keys
-#export -p > $HOME/env.txt
-#ssh -R casadidebug:22:localhost:22 serveo.net
+(while true ; do sleep 60 ; echo "ping" ; done ) &
+ssh-keyscan serveo.net >> ~/.ssh/known_hosts
+echo -e "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDNPkKyOfNMobbgIqD0WvkBTzhEs6St8GYC7aYPT4C0eMimuUBJoQbuUeZQS3hkk9RcdG6h8z0Da70mMV82rcWgztE0obdULduOqDV0GrL6TiqaxoHGTEHPzkq7G48B48S+kmQba0rqURtMJ6SLdr/jElJhnVFC2SC8cYz4Q5BikcuX16LNhxLTxcWSq9Ug9cGO/Gc65n2tKUqPy9Ky4LEURBF3zmUftUdR7wE0GUahsti1aDAQGNbB2ccrpUZWQkVxTzol4ABaCEDo1wBPuug4CBuy+kaIj2aDi01z5D52ED2jsa+g38PStV8zJFFrJJMY10RwigMhHVoOfd7bMF3H jgillis@jglab-work" >> ~/.ssh/authorized_keys
+export -p > $HOME/env.txt
+ssh -R casadidebug:22:localhost:22 serveo.net
 
 sudo mount -t davfs -o ro https://drives.kuleuven.be/hcwebdav/ /mnt/dav/
 
